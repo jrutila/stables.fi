12fc91dc9a87c5bac3ae3b0d78390f3ce1421ad3
Juho Rutila <juho.rutila@iki.fi>
2013-08-23 22:41:03 +0300

    Add stables dependency.

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(
     author='Juho Rutila',
     author_email='juho.rutila@iki.fi',
     url='',
-    install_requires=[ 'Django==1.5', 'django-tenant-schemas', 'South==0.8.1', 'django-reusableapps==0.1.1', 'djangorestframework', 'django-grappelli' ],
+    install_requires=[ 'Django==1.5', 'django-tenant-schemas', 'South==0.8.1', 'django-reusableapps==0.1.1', 'djangorestframework', 'django-grappelli', 'django-stables==dev', 'django-compressor' ],
     zip_ok=False,
+    dependency_links = [
+        "https://api.github.com/repos/jrutila/django-stables/tarball/multitenant?access_token=30e539ba9491700b201ccbeda82b8cb722c4064c#egg=django-stables-dev"
+    ]
 )
