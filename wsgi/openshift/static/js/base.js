90ddc5419e0a6846791fc49774b1c65ca4e93761
Juho Rutila <juho.rutila@iki.fi>
2013-10-19 22:56:31 +0300

    Datepicker subnavbar for calendar.

--- wsgi/openshift/static/js/base.js
+++ wsgi/openshift/static/js/base.js
@@ -12,5 +12,80 @@ $(function () {
     $(document).ready(function() {
         $(".wide").parent(".container").removeClass('container').addClass('dashboard-container')
     })
+
+
+    var selectCurrentWeek = function(that) {
+        window.setTimeout(function () {
+            $(that).find('.ui-datepicker-current-day a').addClass('ui-state-active')
+        }, 1);
+    }
+
+    var collapseToCurrentWeek = function(that) {
+        window.setTimeout(function () {
+            $(that).find("tbody tr:not(:has(.ui-datepicker-current-day))").hide()
+            var is_caret = $(that).find(".ui-datepicker-title .caret").show()
+            if (is_caret.length == 0)
+                $(that).find(".ui-datepicker-year").after("<b class='caret'></b>")
+        }, 1);
+    }
+
+    var startDate;
+    var endDate;
+
+    $('.weekdate-picker').datepicker( {
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        showWeek: true,
+        onSelect: function(dateText, inst) {
+            var date = $(this).datepicker('getDate');
+            var dateFormat = inst.settings.dateFormat || $.datepicker._defaults.dateFormat;
+            if (! $(this).datepicker("option", "selectDate"))
+            {
+                startDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay());
+                endDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + 6);
+
+                selectCurrentWeek(this);
+                collapseToCurrentWeek(this);
+            } else {
+                var formatted = $.datepicker.formatDate("yy-mm-dd", date);
+                if (typeof dashboard_router != 'undefined')
+                    dashboard_router.navigate(formatted, {trigger:true});
+                else
+                    window.location=dashboardUrl+"#"+formatted
+                collapseToCurrentWeek(this);
+            }
+            //$('#endDate').text($.datepicker.formatDate( dateFormat, endDate, inst.settings ));
+        },
+        beforeShowDay: function(date) {
+            var cssClass = '';
+            if(date >= startDate && date <= endDate)
+                cssClass = 'ui-datepicker-current-day';
+            return [true, cssClass];
+        },
+        onChangeMonthYear: function(year, month, inst) {
+            selectCurrentWeek(this);
+        }
+    });
+
+
+
+    collapseToCurrentWeek($('#dashboard-picker'))
+    $('#dashboard-picker').datepicker("option", "selectDate", true );
+    $('#dashboard-picker').datepicker("setDate", new Date(Backbone.history.fragment) );
+    $("#dashboard-picker").datepicker($.datepicker.regional[ "fi" ] );
+    $(".weekdate-picker .ui-datepicker-title").live("click", function() {
+        $(this).parents(".ui-datepicker").find("tbody tr").show()
+        $(this).find(".caret").hide()
+    })
+
+    $('.weekdate-picker .ui-datepicker-calendar tr').live('mousemove', function() { $(this).find('td a').addClass('ui-state-hover'); });
+    $('.weekdate-picker .ui-datepicker-calendar tr').live('mouseleave', function() { $(this).find('td a').removeClass('ui-state-hover'); });
+
+    $('.mainnav li:eq(0)').live('mousemove', function() {
+        $(this).parents(".subnavbar-inner").next(".subnavbar-inner").removeClass("collapse");
+    });
+    $('.subnavbar:has(li:eq(0):not(.active))').on('mouseleave', function() {
+        $(this).find(".subnavbar-inner:eq(1)").addClass("collapse");
+    });
 });
 
--- wsgi/openshift/tenant/templatetags/tags.py
+++ wsgi/openshift/tenant/templatetags/tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.base import Node, NodeList, TemplateSyntaxError
 register = template.Library()
 
 from django.core.urlresolvers import reverse
@@ -8,3 +9,66 @@ def active(request, pattern):
     if reverse(pattern) in request.path:
         return 'class="active"'
     return ''
+
+def do_ifactive(parser, token, negate):
+    bits = list(token.split_contents())
+    if len(bits) != 3:
+        raise TemplateSyntaxError("%r takes two arguments" % bits[0])
+    end_tag = 'end' + bits[0]
+    nodelist_true = parser.parse(('else', end_tag))
+    token = parser.next_token()
+    if token.contents == 'else':
+        nodelist_false = parser.parse((end_tag,))
+        parser.delete_first_token()
+    else:
+        nodelist_false = NodeList()
+    val1 = parser.compile_filter(bits[1])
+    val2 = parser.compile_filter(bits[2])
+    return IfActiveNode(val1, val2, nodelist_true, nodelist_false, negate)
+
+@register.tag
+def ifactive(parser, token):
+    """
+    Outputs the contents of the block if the two arguments equal each other.
+
+    Examples::
+
+        {% ifactive request "home" %}
+            ...
+        {% endifactive %}
+
+    """
+    return do_ifactive(parser, token, False)
+
+@register.tag
+def ifnotactive(parser, token):
+    """
+    Outputs the contents of the block if the two arguments equal each other.
+
+    Examples::
+
+        {% ifnotactive request "home" %}
+            ...
+        {% endifnotactive %}
+
+    """
+    return do_ifactive(parser, token, True)
+
+class IfActiveNode(Node):
+    child_nodelists = ('nodelist_true', 'nodelist_false')
+
+    def __init__(self, var1, var2, nodelist_true, nodelist_false, negate):
+        self.var1, self.var2 = var1, var2
+        self.nodelist_true, self.nodelist_false = nodelist_true, nodelist_false
+        self.negate = negate
+
+    def __repr__(self):
+        return "<IfActiveNode>"
+
+    def render(self, context):
+        val1 = self.var1.resolve(context, True)
+        val2 = self.var2.resolve(context, True)
+        is_active = reverse(val2) in val1.path
+        if (not self.negate and is_active) or (self.negate and not is_active):
+            return self.nodelist_true.render(context)
+        return self.nodelist_false.render(context)
