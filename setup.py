e23909198ed3d651098dd4b2388165f4d565f88f
Juho Rutila <juho.rutila@iki.fi>
2013-11-18 21:37:06 +0200

    Added custom repo for tenant-schemas.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     zip_ok=False,
     zip_safe=False,
     dependency_links = [
-        "https://api.github.com/repos/jrutila/django-stables/tarball/multitenant?access_token=30e539ba9491700b201ccbeda82b8cb722c4064c#egg=django-stables-dev"
+        "https://api.github.com/repos/jrutila/django-stables/tarball/multitenant?access_token=30e539ba9491700b201ccbeda82b8cb722c4064c#egg=django-stables-dev",
+        "https://api.github.com/repos/jrutila/django-tenant-schemas/tarball/master?access_token=30e539ba9491700b201ccbeda82b8cb722c4064c#egg=django-tenant-schemas-1.4.2-1",
     ]
 )
