f27e8032fb1e11d294046178a80032d4c29ff2fa
Juho Rutila <juho.rutila@iki.fi>
2014-02-19 23:36:47 +0200

    Use tenant_context instead of schema_context.

--- wsgi/openshift/tenant/backends.py
+++ wsgi/openshift/tenant/backends.py
@@ -1,13 +1,18 @@
 from django.contrib.auth.backends import ModelBackend
-from tenant_schemas.utils import schema_context, get_public_schema_name
+from tenant_schemas.utils import get_public_schema_name
+from tenant_schemas.utils import get_tenant_model
+from tenant_schemas.utils import tenant_context
+
+def fake_public_tenant():
+    return get_tenant_model()(schema_name=get_public_schema_name())
 
 class MasterUserBackend(ModelBackend):
     def authenticate(self, username=None, password=None):
-        with schema_context(get_public_schema_name()):
+        with tenant_context(fake_public_tenant()):
             ret = super(MasterUserBackend, self).authenticate(username, password)
         return ret
 
     def get_user(self, user_id):
-        with schema_context(get_public_schema_name()):
+        with tenant_context(fake_public_tenant()):
             ret = super(MasterUserBackend, self).get_user(user_id)
         return ret
--- wsgi/openshift/tenant/session/public.py
+++ wsgi/openshift/tenant/session/public.py
@@ -1,23 +1,28 @@
 from django.contrib.sessions.backends.cached_db import SessionStore as CachedStore
-from tenant_schemas.utils import schema_context, get_public_schema_name
+from tenant_schemas.utils import get_public_schema_name
+from tenant_schemas.utils import get_tenant_model
+from tenant_schemas.utils import tenant_context
+
+def fake_public_tenant():
+    return get_tenant_model()(schema_name=get_public_schema_name())
 
 class SessionStore(CachedStore):
     def load(self):
-        with schema_context(get_public_schema_name()):
+        with tenant_context(fake_public_tenant()):
             ret = super(CachedStore, self).load()
         return ret
 
     def save(self, must_create=False):
-        with schema_context(get_public_schema_name()):
+        with tenant_context(fake_public_tenant()):
             ret = super(CachedStore, self).save(must_create)
         return ret
 
     def delete(self, session_key=None):
-        with schema_context(get_public_schema_name()):
+        with tenant_context(fake_public_tenant()):
             ret = super(CachedStore, self).delete(session_key)
         return ret
 
     def flush(self):
-        with schema_context(get_public_schema_name()):
+        with tenant_context(fake_public_tenant()):
             ret = super(CachedStore, self).flush()
         return ret
