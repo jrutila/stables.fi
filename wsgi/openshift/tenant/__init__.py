4f67362bbad5e9b2ab4e6dfe550339c728b90d9c
Juho Rutila <juho.rutila@iki.fi>
2014-02-25 00:20:51 +0200

    Fixed authentication models.

--- wsgi/openshift/tenant/__init__.py
+++ wsgi/openshift/tenant/__init__.py
@@ -2,6 +2,9 @@ from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in
 from django import db
 from django.contrib.auth.models import User
+from tenant_schemas.utils import get_tenant_model
+from tenant_schemas.utils import get_public_schema_name
+from tenant_schemas.utils import tenant_context
 
 TENANT_KEY='_tenant_id'
 
@@ -11,4 +14,7 @@ def addTenantToSession(**kwargs):
         User.objects.get(pk=kwargs['user'].pk, username=kwargs['user'].username)
         kwargs['request'].session[TENANT_KEY] = db.connection.tenant.id
     except User.DoesNotExist:
-        pass
+        tenant = get_tenant_model().objects.get(schema_name=get_public_schema_name())
+        with tenant_context(tenant):
+            User.objects.get(pk=kwargs['user'].pk, username=kwargs['user'].username)
+        kwargs['request'].session[TENANT_KEY] = tenant.id
--- wsgi/openshift/tenant/middleware.py
+++ wsgi/openshift/tenant/middleware.py
@@ -8,17 +8,21 @@ from tenant import TENANT_KEY
 from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseForbidden
+from django.contrib.auth.models import AnonymousUser
 
 def get_user(request):
     if not hasattr(request, '_cached_user'):
         if TENANT_KEY in request.session:
-            with tenant_context(get_tenant_model().objects.get(pk=request.session[TENANT_KEY])):
-                request._cached_user = auth.get_user(request)
-        elif db.connection.schema_name == get_public_schema_name():
-            with tenant_context(get_tenant_model().objects.get(schema_name=get_public_schema_name())):
-                request._cached_user = auth.get_user(request)
+            tenant = get_tenant_model().objects.get(pk=request.session[TENANT_KEY])
+            if tenant.schema_name == get_public_schema_name() or db.connection.schema_name == get_public_schema_name():
+                with tenant_context(tenant):
+                    request._cached_user = auth.get_user(request)
+            elif db.connection.tenant == tenant:
+                    request._cached_user = auth.get_user(request)
+            else:
+                request._cached_user = AnonymousUser()
         else:
-            request._cached_user = auth.get_user(request)
+            request._cached_user = AnonymousUser()
     return request._cached_user
 
 
@@ -37,9 +41,12 @@ class RestrictTenantStaffToAdminMiddleware(object):
                 " MIDDLEWARE_CLASSES setting to insert"
                 " 'tenant.middleware.AuthenticationMiddleware'"
                 " before the RestrictStaffToAdminMiddleware class.")
+        msg = u'Tenant staff members cannot access the public admin site.'
         if reverse('admin:index') in request.path:
             if TENANT_KEY in request.session:
-                if request.user.is_staff and not request.session[TENANT_KEY] == db.connection.tenant.id:
-                    msg = u'Tenant staff members cannot access the public admin site.'
+                tenant = get_tenant_model().objects.get(pk=request.session[TENANT_KEY])
+                if request.user.is_staff and tenant.schema_name != get_public_schema_name() and not request.session[TENANT_KEY] == db.connection.tenant.id:
                     return HttpResponseForbidden(msg)
+            else:
+                return HttpResponseForbidden(msg)
         
