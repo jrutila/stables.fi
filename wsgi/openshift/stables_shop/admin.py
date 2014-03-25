a570e9fc94f4113662c05baa7a6d2ad6d47018e7
Juho Rutila <juho.rutila@iki.fi>
2014-03-25 07:15:56 +0200

    Added EnrollProduct.

--- wsgi/openshift/stables_shop/admin.py
+++ wsgi/openshift/stables_shop/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from models import TicketProduct
 from models import TicketProductActivator
+from models import EnrollProduct
+from models import EnrollProductActivator
 
 #if tenant_schemas.utils.connection.get_tenant().schema_name == 'public':
 admin.site.register(TicketProduct)
 admin.site.register(TicketProductActivator)
+admin.site.register(EnrollProduct)
+admin.site.register(EnrollProductActivator)
